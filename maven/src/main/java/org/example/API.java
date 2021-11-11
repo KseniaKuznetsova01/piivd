abb1b15e2d75f7f316d9487f87aee580a4818f79
“Your <k.kuznetsova4@g.nsu.ru>
2021-11-11 11:59:01 +0700

    Merge pull request #2 from KseniaKuznetsova01/developer
    Part 2(have some problems? but homework is done)

--- maven/src/main/java/org/example/API.java
+++ maven/src/main/java/org/example/API.java
@@ -1,4 +1,55 @@
 package org.example;
+import kong.unirest.Unirest;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 public class API {
+
+    private String date = "";
+    private String currencyValue = "";
+
+    public API(String data, String value) throws DataNotFoundException {
+        if ((data.isBlank()) || (value.isBlank())) {
+            throw new DataNotFoundException();
+        }
+        this.date = data;
+        this.currencyValue = value.toUpperCase();
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getCurrencyName() {
+        return currencyValue;
+    }
+    public void getRate() throws ParserConfigurationException, IOException, DataNotFoundException, SAXException {
+        String url = "https://www.cbr.ru/scripts/XML_daily.asp?date_req=" + date;
+        String data = Unirest.get(url).asString().getBody();
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new InputSource(new StringReader(data)));
+
+
+        NodeList webList = doc.getElementsByTagName("Valute");
+        for (int i = 0; i < webList.getLength(); i++) {
+            Element cur = (Element) webList.item(i);
+            if (cur.getElementsByTagName("CharCode").item(0).getTextContent().equals(currencyValue)) {
+                String value = cur.getElementsByTagName("Name").item(0).getTextContent();
+                String crnc = cur.getElementsByTagName("Value").item(0).getTextContent();
+                System.out.println("\n1 " + value + " = " + crnc + " Российских рублей");
+                return;
+            }
+        }
+        throw new DataNotFoundException();
+    }
 }
--- maven/src/main/java/org/example/Main.java
+++ maven/src/main/java/org/example/Main.java
@@ -1,10 +1,33 @@
 package org.example;
+import org.xml.sax.SAXException;
+
 import java.io.IOException;
 import java.util.Scanner;
 import javax.xml.parsers.ParserConfigurationException;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello world");
+    public static void main(String[] args) throws  IOException {
+        Scanner in = new Scanner(System.in);
+
+        String answer = "1";
+        while (answer.equals("1")) {
+            System.out.println("\nВведите дату в формате dd/MM/yyyy, например, 21/02/2019: ");
+            String data = in.nextLine();
+            System.out.println("Введите идентификатор валюты, например, USD: ");
+            String currencyValue = in.nextLine();
+
+            try {
+                API currency = new API(data, currencyValue);
+                currency.getRate();
+            } catch (DataNotFoundException e) {
+                e.printStackTrace();
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            } catch (SAXException e) {
+                e.printStackTrace();
+            }
+            System.out.print("\nЕсли хотите выйти, нажмите 0, иначе 1");
+            answer = in.nextLine();
+        }
     }
 }
